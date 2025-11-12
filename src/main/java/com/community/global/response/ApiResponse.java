c392b5860ccf1a94a9df91ff74e94d643753bd22
Han <hkjbrian@gmail.com>
2025-11-12 13:28:21 +0900

    feat: 공통 응답 형식 수정 - null 인 경우에도 json 에 포함하도록 수정

--- src/main/java/com/community/global/response/ApiResponse.java
+++ src/main/java/com/community/global/response/ApiResponse.java
@@ -1,10 +1,8 @@
 package com.community.global.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
     private final String message;
