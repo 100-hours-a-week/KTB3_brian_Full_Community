d2180c4d8fc464e7985f90de8d98202c29e946ba
Han <hkjbrian@gmail.com>
2025-11-21 10:35:22 +0900

    feat: JwtFilter 추가 - TokenProvider 에게 토큰 검증 처리 위임

---
+++ src/main/java/com/community/global/security/JwtFilter.java
@@ -0,0 +1,35 @@
+package com.community.global.security;
+
+import com.community.domain.auth.service.TokenProvider;
+import com.community.global.exception.CustomException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+            String token = tokenProvider.resolveToken(request);
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
