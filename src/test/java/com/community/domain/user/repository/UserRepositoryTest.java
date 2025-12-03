9039ddfb6f32892606fcf5bf58e029602a1e3c53
Han <hkjbrian@gmail.com>
2025-12-03 14:05:35 +0900

    test: UserRepository 테스트 코드 자동 설정 제외

--- src/test/java/com/community/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/com/community/domain/user/repository/UserRepositoryTest.java
@@ -10,12 +10,14 @@ import com.community.domain.user.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @Import(JpaUserRepository.class)
 public class UserRepositoryTest {
